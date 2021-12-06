ee247b64c54c8b3df2d3a55bb41fa64e237cd412
mikairyuu <miva200267@gmail.com>
2021-12-06 13:44:47 +1000

    API placeholder

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Druzhbank.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+        
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("login")]
+        public ActionResult<string> Login()
+        {
+            return "Здесь будет осуществляться логин в банк";
+        }
+
+
+    }
+}
